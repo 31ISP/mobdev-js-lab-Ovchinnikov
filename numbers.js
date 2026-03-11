dc51603fe4795296f17788ca858228f2e9ef957d
spromt300-jpg <spromt300@gmail.com>
2026-03-11 15:18:32 +0000

    finished work

--- numbers.js
+++ numbers.js
@@ -4,7 +4,10 @@ console.log(temperature.toFixed(1));
 const a = 17, b = 5;
 console.log(Math.floor(a%b));
 // ====================================
-
+const min = 1;
+const max = 10;
+const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(randomNumber);
 // ====================================
 console.log(`isNaN("100"/ 5) : ${isNaN("100"/ 5)}; isNaN("сто"/ 5) : ${isNaN("сто"/ 5)}`);
 // ====================================
